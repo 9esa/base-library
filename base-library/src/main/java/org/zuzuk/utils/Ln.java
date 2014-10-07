afec1aa52f1aae68d9032a2207b00c51f5998e01
Gavriil Sitnikov <zuzuk666@gmail.com>
2014-10-07 17:45:54 +0400

    Ln class update

--- base-library/src/main/java/org/zuzuk/utils/Ln.java
+++ base-library/src/main/java/org/zuzuk/utils/Ln.java
@@ -1,5 +1,6 @@
 package org.zuzuk.utils;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 /**
@@ -7,6 +8,12 @@ import android.util.Log;
  * Simple logger using caller class name as tag
  */
 public class Ln {
+    private static int LogLevel = Log.ERROR;
+
+    /* Sets log level */
+    public static void setLogLevel(int logLevel) {
+        LogLevel = logLevel;
+    }
 
     private static String getTag() {
         StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
@@ -15,36 +22,43 @@ public class Ln {
 
     /* Verbose level log */
     public static void v(String message) {
-        if (Log.isLoggable("Ln", Log.VERBOSE)) {
+        if (LogLevel <= Log.VERBOSE) {
             Log.v(getTag(), message);
         }
     }
 
     /* Debug level log */
     public static void d(String message) {
-        if (Log.isLoggable("Ln", Log.DEBUG)) {
+        if (LogLevel <= Log.DEBUG) {
             Log.d(getTag(), message);
         }
     }
 
     /* Info level log */
     public static void i(String message) {
-        if (Log.isLoggable("Ln", Log.INFO)) {
+        if (LogLevel <= Log.INFO) {
             Log.i(getTag(), message);
         }
     }
 
     /* Warning level log */
     public static void w(String message) {
-        if (Log.isLoggable("Ln", Log.WARN)) {
+        if (LogLevel <= Log.WARN) {
             Log.w(getTag(), message);
         }
     }
 
     /* Error level log */
     public static void e(String message) {
-        if (Log.isLoggable("Ln", Log.ERROR)) {
+        if (LogLevel <= Log.ERROR) {
             Log.e(getTag(), message);
+        }
+    }
+
+    /* Prints stack trace in log with DEBUG level */
+    public static void printStackTrace(String tag) {
+        if (LogLevel <= Log.DEBUG) {
+            Log.d(tag, TextUtils.join("\n", Thread.currentThread().getStackTrace()));
         }
     }
 }
