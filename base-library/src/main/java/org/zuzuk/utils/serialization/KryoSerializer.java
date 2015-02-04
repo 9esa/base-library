c600ecf22afb765df82f0cee821179de9ee8efde
Vsevolod Ivanov <seva.ask@gmail.com>
2015-02-04 20:21:30 +0300

    kryo working

--- base-library/src/main/java/org/zuzuk/utils/serialization/KryoSerializer.java
+++ base-library/src/main/java/org/zuzuk/utils/serialization/KryoSerializer.java
@@ -4,9 +4,7 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 public enum KryoSerializer implements Serializer {
 
@@ -15,22 +13,17 @@ public enum KryoSerializer implements Serializer {
     private static ThreadLocal<Kryo> kryoThreadLocal = new ThreadLocal<Kryo>() {
         @Override
         public Kryo initialValue() {
-            Kryo kryo = new Kryo();
-            return kryo;
+            return new Kryo();
         }
     };
 
     @Override
     public <TObject> byte[] serialize(TObject object) {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(4000);
-        kryoThreadLocal.get().writeClassAndObject(new Output(byteArrayOutputStream), object);
-        try {
-            byteArrayOutputStream.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        byte[] bytes = byteArrayOutputStream.toByteArray();
-        return bytes;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Output output = new Output(byteArrayOutputStream);
+        kryoThreadLocal.get().writeClassAndObject(output, object);
+        output.close();
+        return byteArrayOutputStream.toByteArray();
     }
 
     @Override
